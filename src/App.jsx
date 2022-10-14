b94673b39950e94eec5894d8aea401ccac84597f
wpbreak <jesusgp_23_98@outlook.es>
2022-10-14 17:02:06 -0500

    update tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import TaskList from './TaskList'
-import TaskForm from './TaskForm'
-import {tasks as data} from './tasks'
-import { useState, useEffect } from 'react'
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import { tasks as data } from "./data/tasks";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
 
-  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    setTasks(data);
+  }, []);
 
-    useEffect(() => {
-      setTasks(data)
-    }, [])
-
-    function createTask(titleTask) {
-      setTasks([...tasks, {
-        title: titleTask,
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
         id: tasks.length,
-        description: 'nueva tarea'
-      }])
-    }
+        description: task.description,
+      },
+    ]);
+  }
 
   return (
     <>
-      <TaskForm createTask={createTask}/>
-      <TaskList tasks={tasks}/>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} />
     </>
-  )
+  );
 }
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 export default function TaskForm({createTask}) {
 
     const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        createTask(title)
+        createTask({title, description})
+    setTitle('')
+    setDescription('')
     }
 
   return (
@@ -15,7 +18,14 @@ export default function TaskForm({createTask}) {
       <input 
       placeholder="Escribe tu tarea"
       onChange={(e) => {setTitle(e.target.value)}}
+      value= {title}
+      autoFocus
       />
+      <textarea 
+      placeholder="Descripcion de la tarea"
+      onChange={(e) => {setDescription(e.target.value)}}
+      value= {description}
+      ></textarea>
       <button>Guardar</button>
     </form>
   )
