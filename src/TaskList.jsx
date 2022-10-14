28f2d054fec8842d4196bbfb8634c0d0a34b70e4
Jesus Garcia - vn54gzt <Jesus.Garcia4@walmart.com>
2022-10-14 14:41:16 -0500

    first commit

---
+++ src/TaskList.jsx
@@ -0,0 +1,20 @@
+function TaskList({tasks}) {
+
+    if (tasks.length == 0) {
+        return <h1>No hay tareas</h1>
+    }
+
+  return (
+    <>
+        {
+            tasks.map((task) => (
+            <div key={task.id}>
+                <h1>{task.title}</h1>
+                <p>{task.description}</p>
+            </div>))
+        }
+    </>
+  )
+}
+
+export default TaskList
